6c728b582ca81c3644ff863513c0e3ae144c3827
zerialfan <zerialfan@gmail.com>
2024-01-12 05:54:20 -0600

    new project

--- game.js
+++ game.js
@@ -1,33 +1,3 @@
 const canvas = document.getElementById('game')
-const cxt = canvas.getContext("2d")
+const ctx = canvas.getContext('2d')
 
-// Le agrego un eventlistner a window load
-
-window.addEventListener('resize',handleResize)
-window.addEventListener('load',handleResize)
-
-function handleResize() {
-    reSize()
-    startGame()
-}
-function reSize() {
-    const isVertical =window.innerHeight > window.innerWidth  
-    let newSize= isVertical ? window.Math.ceil(innerWidth * 0.9) : Math.floor(window.innerHeight*0.7)
-    let canvasSize = canvas.width = canvas.height = newSize
-    return canvasSize
-}
-function startGame() {
-    const canvasSize = reSize()
-    
-    const elementSize = Math.floor(canvasSize /10) 
-    const spacing = Math.floor(elementSize*.1) 
-    for (let i = 1;  i< 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            let x = j*elementSize -spacing
-            let y = i* elementSize+ i*spacing
-            cxt.font = elementSize+'px Arial'
-            cxt.fillText(emojis['X'],x,y)
-        }
-    }
-    console.log({ canvasSize, elementSize }) 
-}
